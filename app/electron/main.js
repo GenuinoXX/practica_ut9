0a44f9ac095ca86913d1cb8bdf6d73daa33d4e36
GenuinoXX <maurisebas2000@gmail.com>
2025-02-27 08:35:28 +0100

    Segunda ventana y activar

--- app/electron/main.js
+++ app/electron/main.js
@@ -2,6 +2,7 @@ const { app, BrowserWindow, ipcMain } = require("electron")
 const path = require("path")
 
 let ventanaPrincipal
+let segundaVentana
 
 app.whenReady().then(() => {
     ventanaPrincipal = new BrowserWindow({
@@ -15,6 +16,27 @@ app.whenReady().then(() => {
     })
 
     ventanaPrincipal.loadURL("http://localhost:3001")
+
+    segundaVentana = new BrowserWindow({
+        width: 600,
+        height: 400,
+        parent: mainWindow,
+        webPreferences: {
+            preload: path.join(__dirname, "preload.js"),
+            nodeIntegration: false,
+            contextIsolation: true,
+        },
+    })
+
+    segundaVentana.loadURL("http://localhost:3001/perfil/mis_recetas");
+
+    app.on("activate", () => {
+        if (BrowserWindow.getAllWindows().length === 0) {
+            app.whenReady().then(() => {
+                ventanaPrincipal.show();
+            });
+        }
+    });
 })
 
 app.on("window-all-closed", () => {
