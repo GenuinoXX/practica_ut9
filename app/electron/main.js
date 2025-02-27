8bd6669121d403d275b192d7d670d43d61394dc7
GenuinoXX <maurisebas2000@gmail.com>
2025-02-27 16:04:47 +0100

    He refactoriado el código

--- app/electron/main.js
+++ app/electron/main.js
@@ -1,24 +1,31 @@
-const { app, BrowserWindow, ipcMain } = require("electron")
+const { app, BrowserWindow } = require("electron")
 const path = require("path")
 
-let ventanaPrincipal
-
-app.whenReady().then(() => {
-    ventanaPrincipal = new BrowserWindow({
+function nuevaVentana() {
+    let ventanaPrincipal = new BrowserWindow({
         width: 1200,
         height: 800,
         webPreferences: {
-            preload: path.join(__dirname, "preload.js"),
             nodeIntegration: false,
             contextIsolation: true,
         },
     })
 
     ventanaPrincipal.loadURL("http://localhost:3001")
+}
+
+app.whenReady().then(() => {
+    nuevaVentana()
 })
+
+app.on("activate", () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+        nuevaVentana()
+    }
+});
 
 app.on("window-all-closed", () => {
     if (process.platform !== "darwin") {
-        app.quit();
+        app.quit()
     }
 })
